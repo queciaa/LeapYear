4e8718f9611b4865d8027136fb659de8dc4f2c61
queciaa <114770554+queciaa@users.noreply.github.com>
2023-02-24 15:39:23 +0300

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("please enter a year ");
+        int year = scan.nextInt();
+
+        if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 == 0))) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+        scan.close();
+    }
+}
